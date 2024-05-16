e92721eea374d355799c3c3ea56bc316632b3746
ngocpd-1250 <phan.duong.ngoc@sun-asterisk.com>
2024-05-16 13:43:23 +0700

    Initial Commit

---
+++ TCA_example/TCA_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_exampleApp.swift
+//  TCA_example
+//
+//  Created by phan.duong.ngoc on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCA_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
